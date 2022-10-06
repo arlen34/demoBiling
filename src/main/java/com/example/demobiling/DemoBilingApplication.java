fd92dfb9e89139d179022c82d16ba74a19d0adf5
arlen34 <arlen.kubanychbekovv01@gmail.com>
2022-10-06 19:00:55 +0300

    Initial commit

---
+++ src/main/java/com/example/demobiling/DemoBilingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBilingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBilingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demobiling/DemoBilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBilingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
